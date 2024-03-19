46f23400cfcd659f4cda0fbf8a75ae5459a8a208
Shikhar <shikhar.sri23@gmail.com>
2024-03-19 09:56:51 +0530

    final-commit

--- main.js
+++ main.js
@@ -1,9 +1,101 @@
-import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMapping.js";
+import { getWeather } from "./weather.js";
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
-  .then((resp) => {
-    console.log(resp.data);
-  })
-  .catch((error) => {
-    console.error("Error fetching data: ", error);
+//getting the user's current location
+navigator.geolocation.getCurrentPosition(positionSuccess, positionFailure);
+
+function positionSuccess({ coords }) {
+  getWeather(
+    coords.latitude,
+    coords.longitude,
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  )
+    .then(renderWeather)
+    .catch((error) => {
+      console.error(error);
+      alert("Error fetching weather");
+    });
+}
+
+function positionFailure() {
+  alert("There was an error fetching your location");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+  document.body.classList.remove("blurred");
+}
+
+function getIconUrl(iconCode) {
+  return `./icons/${ICON_MAP.get(iconCode)}.svg`;
+}
+
+function renderCurrentWeather(current) {
+  document.querySelector("[data-current-icon]").src = getIconUrl(
+    current.iconCode
+  );
+
+  // document.querySelector("[data-current-temp]").textContent =
+  //   current.currentTemp;
+
+  setValue("current-temp", current.currentTemp);
+
+  // document.querySelector("[data-current-high]").textContent = current.HighTemp;
+
+  setValue("current-high", current.HighTemp);
+
+  document.querySelector("[data-current-low]").textContent = current.LowTemp;
+
+  document.querySelector("[data-current-fl-low]").textContent =
+    current.feelsLikeLowTemp;
+
+  document.querySelector("[data-current-fl-high]").textContent =
+    current.feelsLikeHighTemp;
+
+  document.querySelector("[data-current-wind]").textContent = current.windSpeed;
+
+  document.querySelector("[data-current-precip]").textContent = current.precip;
+}
+
+const dailySection = document.querySelector(".days-section");
+const dayCardTemplate = document.getElementById("day-card-template");
+
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const dayCard = dayCardTemplate.content.cloneNode(true);
+    setValue("day", day.day, { parent: dayCard });
+    setValue("temp", day.maxTemp, { parent: dayCard });
+    dayCard.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+
+    dailySection.append(dayCard);
   });
+}
+
+const hourSection = document.querySelector(".hour-section");
+const hourRowTemplate = document.getElementById("hour-row-template");
+
+function renderHourlyWeather(hourly) {
+  hourSection.innerHTML = "";
+
+  hourly.forEach((hour) => {
+    //creating a cloned row for row template
+    const hourRow = hourRowTemplate.content.cloneNode(true);
+    hourRow.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    setValue("day", hour.day, { parent: hourRow });
+    setValue("time", hour.time, { parent: hourRow });
+
+    setValue("temp", hour.currTemp, { parent: hourRow });
+    setValue("fl-temp", hour.feelsLikeTemp, { parent: hourRow });
+    setValue("wind", hour.windSpeed, { parent: hourRow });
+    setValue("precip", hour.precipitation, { parent: hourRow });
+
+    hourSection.appendChild(hourRow);
+  });
+}
--- weather.js
+++ weather.js
@@ -1,16 +1,113 @@
-import axios from "axios";
+//import axios from "./node_modules/axios/index.js";
 
 //https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&start_date=2024-03-14&end_date=2024-03-14
 
 export function getWeather(lat, lon, timezone) {
-  return axios.get(
-    "https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&start_date=2024-03-14&end_date=2024-03-14",
-    {
-      params: {
-        latitude: lat,
-        longitude: lon,
-        timezone,
-      },
-    }
-  );
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?current=temperature_2m,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&wind_speed_unit=mph&precipitation_unit=inch",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone,
+          start_date: getCurrentDate().slice(0, 10),
+          end_date: getEndDate(),
+        },
+      }
+    )
+    .then(({ data }) => {
+      //return data;
+      return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
+      };
+    });
+}
+
+function parseCurrentWeather({ current, daily }) {
+  const {
+    precipitation: precip,
+    wind_speed_10m: windSpeed,
+    weather_code: iconCode,
+    temperature_2m: currentTemp,
+  } = current;
+
+  const {
+    apparent_temperature_max: [feelsLikeHighTemp],
+    apparent_temperature_min: [feelsLikeLowTemp],
+    temperature_2m_max: [HighTemp],
+    temperature_2m_min: [LowTemp],
+  } = daily;
+
+  return {
+    iconCode,
+    currentTemp: Math.round(currentTemp),
+    HighTemp: Math.round(HighTemp),
+    LowTemp: Math.round(LowTemp),
+    feelsLikeHighTemp: Math.round(feelsLikeHighTemp),
+    feelsLikeLowTemp: Math.round(feelsLikeLowTemp),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+  };
+}
+
+function parseDailyWeather({ daily }) {
+  return daily.time.map((date, index) => {
+    return {
+      day: getDayString(date),
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+      iconCode: daily.weather_code[index],
+    };
+  });
+}
+
+function parseHourlyWeather({ hourly }) {
+  return hourly.time
+    .map((dateTime, index) => {
+      return {
+        day: getDayString(dateTime),
+        time: getTimeHour(dateTime),
+        timeSinceEpoch: new Date(dateTime).getTime(), //time in ms since epoch
+        iconCode: hourly.weather_code[index],
+        currTemp: Math.round(hourly.temperature_2m[index]),
+        feelsLikeTemp: hourly.apparent_temperature[index],
+        windSpeed: Math.round(hourly.wind_speed_10m[index]),
+        precipitation: Math.round(hourly.precipitation[index] * 100) / 100,
+      };
+    })
+    .filter(({ timeSinceEpoch }) => {
+      return timeSinceEpoch >= new Date().getTime();
+    });
+}
+
+function getCurrentDate() {
+  return new Date().toISOString();
+}
+
+function getEndDate() {
+  const currentDate = new Date();
+  const futureDate = new Date(currentDate.getTime() + 6 * 24 * 60 * 60 * 1000);
+  return futureDate.toISOString().slice(0, 10);
+}
+
+function getDayString(date) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[new Date(date).getDay()];
+}
+
+function getTimeHour(dateTime) {
+  return new Date(dateTime).toLocaleTimeString("en-US", {
+    hour12: true,
+    hour: "numeric",
+  });
 }
